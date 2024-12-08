8a2646cc160f0c43dc9dc75f317bf1770a39a379
Jan <MapManagement@protonmail.com>
2024-12-09 00:22:41 +0100

    Approach of solving second puzzle of day 6

--- day6/day6.py
+++ day6/day6.py
@@ -73,7 +73,61 @@ def first_puzzle():
     return count_xes(grid)
     
 def second_puzzle():
-    pass
+    global_grid = read_input()
+    grid_size = (len(global_grid), len(global_grid[0]))
+    global_guard = find_start_of_guard(global_grid)
+
+    count = 0
+
+    for row in range(grid_size[0]):
+        for col in range(grid_size[1]):
+            grid = global_grid.copy()
+            guard = global_guard.copy()
+            visited = [[[guard[0][0], guard[0][1]], guard[1]]]
+            
+            if grid[row][col] in ["#", "^", ">", "v", "<"]:
+                continue
+
+            grid[row][col] = "#"
+
+            for x in grid:
+                print(x)
+
+            a = input()
+
+            while True:
+                guard_row = guard[0][0]
+                guard_col = guard[0][1]
+                guard_direction = guard[1]
+                next_direction = get_next_direction(guard_direction)
+
+                next_guard_row = guard_row
+                next_guard_col = guard_col
+
+                guard_update = guard_markers[guard_direction]
+                next_guard_row += guard_markers[guard_direction][0]
+                next_guard_col += guard_markers[guard_direction][1]
+
+                if is_out_of_field(grid_size, next_guard_row, next_guard_col):
+                    break
+
+                visit = [[next_guard_row, next_guard_col], next_direction]
+
+                if visit in visited:
+                    count += 1
+                    break
+
+                visited.append(visit)
+
+                if grid[next_guard_row][next_guard_col] == "#":
+                    guard[1] = next_direction
+                else:
+                    grid[guard_row][guard_col] = "."
+                    grid[next_guard_row][next_guard_col] = next_direction
+                    guard[0] = [next_guard_row, next_guard_col]
+
+    return count
+
     
 def main():
     print(f"First puzzle: {first_puzzle()}")
